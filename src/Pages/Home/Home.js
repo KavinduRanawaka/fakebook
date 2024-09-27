f9a5033cf1bfcc84ace4026dceac4224929d72bd
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-27 13:24:21 +0530

    home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import NavBar from '../../Components/Navigation/NavBar'
+import LeftPane from '../../Components/LeftPane/LeftPane'
+import RightPane from '../../Components/RightPane/RightPane'
+import PostPane from '../../Components/PostPane/PostPane'
+import "./Home.css";
 
 export default function Home() {
   return (
-    
+    <div>
       <NavBar/>
-    
+
+      <div className="bottomContainer">
+        <LeftPane/>
+        <PostPane/>
+        <RightPane/>
+      </div>
+    </div>
   )
 }
