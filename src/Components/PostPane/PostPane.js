9f3efee8c7b02310261f1b477dc7373a4a4549fb
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-27 13:22:52 +0530

    postpane.js

---
+++ src/Components/PostPane/PostPane.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./PostPane.css"
+
+export default function PostPane() {
+  return (
+    <div className='postPaneBox'>
+      <h1>PostPane</h1>
+    </div>
+  )
+}
