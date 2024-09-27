a17d23e6a89488217127ab658b6677a92526bfdf
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-27 13:23:45 +0530

    rightpane.js

---
+++ src/Components/RightPane/RightPane.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./RightPane.css"
+
+export default function RightPane() {
+  return (
+    <div className='rightPaneBox'>
+      <h1>RightPane</h1>
+    </div>
+  )
+}
